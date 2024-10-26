ede776cb902ccbc24b51d4de0298ce58f67fea66
MosElsh <moselsh1@gmail.com>
2024-10-26 23:55:07 +0100

    Models Completed

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project, Employee, Assignment
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Employee)
+admin.site.register(Assignment)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    background = models.TextField()
+    is_working = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    employees = models.ManyToManyField(Employee, through="Assignment")
+
+    def __str__(self):
+        return f"{self.name} started at {self.start_date}"
+
+class Assignment(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+    weekly_hours = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.employee} is working on {self.project} as a {self.role} for {self.weekly_hours} hours"

396beab56cb9da4a3db3bd5eac3a2dc6f937163a
MosElsh <moselsh1@gmail.com>
2024-10-26 22:38:57 +0100

    Starter Template

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container pt-3">
+        <div class="h1 text-center border rounded bg-light p-2 mb-3">
+            API Client
+        </div>
+        <div class="mb-3">
+            <u>Response data</u>:             
+        </div>
+        <pre>{{ response_data }}</pre>
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+  </template>
+  
+<script>
+export default {
+    data() {
+        return {
+            response_data: '',
+        }
+    },
+    async mounted() {
+        const response = await fetch('http://localhost:8000/api/test.json')
+        this.response_data = await response.json()
+    }
+}
+</script>
